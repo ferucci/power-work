701a9e18a54cc5a80bbce28f8a58a6eb90e6faf4
ferucci <development.first1@gmail.com>
2022-03-24 19:05:30 +0300

    completed the first lesson

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-'use strict';
+// 'use strict';
 
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+console.log('У меня атрибут "defer" не всегда срабатывает. Почему так?');
+
+alert('Я не понял, стоило присваивать переменным какие-нибудь значения или нет?) Если нет - нажмите ОК =)');
