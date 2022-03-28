bf7c6200af4e57a4c32f8ea59884e4fc9d10167a
ferucci <development.first1@gmail.com>
2022-03-28 23:10:20 +0300

    I completed lesson 3

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-// 'use strict';
+'use strict';
 
-let title = 'Frenzied study JavaScript';
-let screens = "Смартфоны (320-480px),Планшеты(768px+),Нетбуки(1024px+),Мониторы OLED/AMOLED (1280px+)";
-let screenPrice = 17;
-let rollback = 55;
-let fullPrice = 42;
-let adaptive = true;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+const title = prompt('Как называется ваш проект?');
 
-console.log(screens.length);
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const cost = prompt('Сколько будет стоить данная работа?', '20000 рублей');
+const screenPrice = parseInt(cost);
 
-console.log('Стоимость верстки экранов', screenPrice + "", 'рублей');
-console.log('Стоимость верстки сайта', fullPrice + "", 'рублей');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 
-console.log(screens.toLowerCase().split(','));
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
 
-console.log(fullPrice * (rollback / 100));
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = fullPrice - ((fullPrice / 100) * 35);
+
+console.log(Math.ceil(servicePercentPrice));
+
+switch (true) {
+  case fullPrice > 30000:
+  case fullPrice === 30000:
+    console.log('Даем скидку в 10%');
+    break;
+  case fullPrice > 15000 && fullPrice < 30000:
+    console.log('Даем скидку в 5%');
+    break;
+  case fullPrice === 15000:
+  case fullPrice > 0 && fullPrice < 15000:
+    console.log('Скидка не предусмотрена');
+    break;
+  case fullPrice === 0:
+  case fullPrice < 0:
+    console.log('Что то пошло не так');
+    break;
+}
+
+
+
