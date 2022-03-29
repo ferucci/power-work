293417049f09310fd06f7822402c7dd826e57b9b
ferucci <development.first1@gmail.com>
2022-03-29 17:29:42 +0300

    fixed edits

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 'use strict';
 
-
-const title = prompt('Как называется ваш проект?');
-
+const title = prompt('Как называется ваш проект?', 'Frenzied study JavaScript');
 const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 const cost = prompt('Сколько будет стоить данная работа?', '20000 рублей');
 const screenPrice = parseInt(cost);
-
 const adaptive = confirm('Нужен ли адаптив на сайте?');
-
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = +prompt('Сколько это будет стоить?');
-
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = +prompt('Сколько это будет стоить?');
-
+let rollback = 35;
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = fullPrice - ((fullPrice / 100) * 35);
-
-console.log(Math.ceil(servicePercentPrice));
+const servicePercentPrice = fullPrice - ((fullPrice / 100) * rollback);
 
 switch (true) {
   case fullPrice >= 30000:
@@ -35,5 +28,17 @@ switch (true) {
     break;
 }
 
+console.log(Math.ceil(servicePercentPrice));
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов', screenPrice + "", 'рублей');
+console.log('Стоимость верстки сайта', fullPrice + "", 'рублей');
+
+console.log(screens.toLowerCase().split(','));
+
+console.log(fullPrice * (rollback / 100));
