ed8cc69e5324afd2c7e50b291007fdc7a20bac98
ferucci <development.first1@gmail.com>
2022-03-30 00:25:15 +0300

    added lesson04

--- script.js
+++ script.js
@@ -1,21 +1,64 @@
 // 'use strict';
 
-let title = 'Frenzied study JavaScript';
-let screens = "Смартфоны (320-480px),Планшеты(768px+),Нетбуки(1024px+),Мониторы OLED/AMOLED (1280px+)";
-let screenPrice = 17;
-let rollback = 55;
-let fullPrice = 42;
-let adaptive = true;
+let title = prompt('Как называется ваш проект?', 'frenzied study JavaScript');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+let cost = prompt('Сколько будет стоить данная работа?', '20000 рублей');
+let screenPrice = parseInt(cost);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+let rollback = 35;
+let fullPrice;
+let allServicePrices;
+let servicePercentPrice;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
 
-console.log(screens.length);
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return 'Даём скидку в 10%';
+  } else if (price >= 15000 && price < 30000) {
+    return 'Даём скидку в 5%';
+  } else if (price >= 0 && price < 15000) {
+    return 'Скидка не предусмотрена';
+  } else {
+    return 'Что-то пошло не так';
+  }
+};
 
-console.log('Стоимость верстки экранов', screenPrice + "", 'рублей');
-console.log('Стоимость верстки сайта', fullPrice + "", 'рублей');
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
+};
+allServicePrices = getAllServicePrices();
+
+function getFullPrice() {
+  return screenPrice + allServicePrices;
+}
+fullPrice = getFullPrice();
+servicePercentPrice = fullPrice - ((fullPrice / 100) * rollback);
+
+const getTitle = function () {
+  if (title[0] !== ' ') {
+    return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+  } else {
+    return title;
+  }
+};
+
+function getServicePercentPrices() {
+  return fullPrice - (fullPrice * (rollback / 100));
+}
+servicePercentPrice = getServicePercentPrices();
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
 
 console.log(screens.toLowerCase().split(','));
+console.log(getRollbackMessage(fullPrice));
+console.log(Math.ceil(getServicePercentPrices()));
 
-console.log(fullPrice * (rollback / 100));
